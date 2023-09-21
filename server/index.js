e79b2c60fbffd706795b1cb38bef51baa0ef1fc4
IbnMikhail <mikhailtoby@gmail.com>
2023-09-21 15:42:50 +0100

    updated the backend

--- server/index.js
+++ server/index.js
@@ -1,14 +1,10 @@
 import express from 'express';
 import cors from "cors"
-
+import sql from "./db.js"
 
 const app = express();
 
 //middleware
-app.use(cors(/*{
-    origin: ["http://localhost:5173"],
-}*/)
-);
 app.use(express.json());
 
 //ROUTES
@@ -16,18 +12,57 @@ app.use(express.json());
 app.post("/todos", async (req,res) => {
     try {
         const {task, is_completed} = req.body;  
-        const todos = await sql `INSERT INTO  * FROM todos (task, is_completed) VALUES (${task}, ${is_completed}) RETURNING *`;
+        const todos = await sql `INSERT INTO  todos (task, is_completed) VALUES (${task}, ${is_completed}) RETURNING *`;
         res.json(todos);
         console.log(todos)
     } catch (error) {
-        console.error(err.message);
+        console.log(error.message);
     }
 })
 //GET ALL TODO
+app.get("/todos", async(req,res) => {
+    try {
+        const allTodos = await sql `SELECT * FROM todos `;
+        res.json(allTodos)
+    } catch (error) {
+        console.error(error.message);
+    }
+})
 //GET A TODO
-//UPDATE A TODO
-//DELETE A TODO
 
+app.get("/todos/:id", async (req,res) => {
+    try {
+        const { id } = req.params;
+        const todo = await sql `SELECT * FROM todos WHERE id = ${id}`;
+        res.json(todo);
+    } catch (error) {
+        console.error(error.message);
+        
+    }
+});
+
+//UPDATE A TODO
+app.put("/todos/:id", async(req,res) => {
+    try {
+      const {id} = req.params;
+      const { task, is_completed} = req.body;
+      const updateTodos = await  sql `UPDATE todos SET task = ${task},is_completed = ${is_completed} WHERE id = ${id} `;
+      res.json("Todos was updated");
+    } catch (error) {
+       console.log(error.message); 
+    }
+})
+//DELETE A TODO
+app.delete("/todos/:id", async (req,res) => {
+    try {
+        const {id} = req.params;
+        const deleteTodo = await sql `DELETE FROM todos WHERE id = ${id}`;
+        res.json("Todo was deleted");
+    } catch (error) {
+        console.log(error.message)
+        
+    }
+})
 app.listen(8000, () => {
     console.log("server has started on port 8000")
 }) 
